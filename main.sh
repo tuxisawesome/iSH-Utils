c5304cf9d4705413a4ce0e5ce41173cd4fc8f51d
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-08-20 10:13:59 -0400

    poosh

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+clear
+echo "iSH Bootstrapper"
+echo "iSH for iPad" > /etc/motd
+
+apk add mc bash openssh vim nano htop wget git neofetch cmatrix gcc g++ cmake make ninja go rust cargo py3-pip
+
+
